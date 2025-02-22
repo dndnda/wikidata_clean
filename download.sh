02ba4119bad7c6c197b26914142b529792379d48
dndnda <1344030328@qq.com>
2025-02-22 20:22:38 +0800

    fix typo

--- download.sh
+++ download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 默认下载链接（根据需要修改）
-DEFAULT_URL="https://dumps.wikimedia.org/zhwiki/20250201/zhwiki-20250201-pages-articles-multistream1.xml-p1p187712.bz2"
+#DEFAULT_URL="https://dumps.wikimedia.org/zhwiki/20250201/zhwiki-20250201-pages-articles-multistream1.xml-p1p187712.bz2"
+DEFAULT_URL="https://dumps.wikimedia.org/zhwiki/20250201/zhwiki-20250201-pages-articles-multistream6.xml-p7096380p8596379.bz2"
 
 # 如果传入参数，则使用该参数作为下载链接，否则使用默认链接
 if [ -n "$1" ]; then
